abe6ef5faa5aaa52c32f6240b339f64c20f0c491
Ben Hilarides <ben.hilarides26@gmail.com>
2025-11-09 11:37:12 -0500

    initial commit taco stand and demos

--- virtual-taco-stand/src/app/app.component.spec.ts
+++ virtual-taco-stand/src/app/app.component.spec.ts
@@ -1,10 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MenuComponent } from './menu/menu.component';
+import { Routes, Router } from '@angular/router';
 
-describe('AppComponent', () => {
+describe('AppComponent (Standalone)', () => {
   beforeEach(async () => {
+    const activatedRouteStub = {
+      snapshot: {
+        paramMap: {
+          get: () => 'staticValue',
+        },
+      },
+      queryParams: of({}),
+    };
+
+    const routes: Routes = [
+      { path: 'menu', component: MenuComponent }
+    ]
+
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule.withRoutes(routes), // Include RouterTestingModule to handle routing
+        MenuComponent  ],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteStub }
+      ]
     }).compileComponents();
   });
 
@@ -25,5 +48,15 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, virtual-taco-stand');
+  });
+
+  /** Week 3: unit test 3 */
+  it('should have correct route for Menu Component', () => {
+    const router = TestBed.inject(Router);
+    const route = router.config.find(r => r.path === 'menu');
+    expect(route).toBeDefined(); // Check that the route is defined
+    if (route) {
+      expect(route.component).toBe(MenuComponent); // Check if the component is MenuComponent
+    }
   });
 });
